2dda16183a404972105d1a7e48a4ba760c1b2044
sathupatisindhu <121924038+sathupatisindhu@users.noreply.github.com>
2023-12-24 10:23:46 +0530

    Create output.tf

---
+++ terraform/modules/eks-nodepool/output.tf
@@ -0,0 +1,14 @@
+# output "cluster_endpoint" {
+#     description = "Endpoints of the API server"
+#     value = [for cluster in aws_eks_cluster.aws_eks : cluster.endpoint] 
+# }
+
+# # output "oidc_url" {
+# #     description = "oidc provider url"
+# #     value = [for url in aws_iam_openid_connect_provider.oidc_cluster : url.url]
+# # }
+
+# # output "eks_cluster_identity_oidc_issuer_arn" {
+# #   description = "The OIDC Identity issuer ARN for the cluster that can be used to associate IAM roles with a service account"
+# #   value = [for arn in aws_iam_openid_connect_provider.oidc_cluster : arn.arn ]
+# # }
